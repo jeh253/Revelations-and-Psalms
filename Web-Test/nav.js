2296dfc9d34b093918c0672a9760d82d95520ef1
Mason Gobat <masongobat@gmail.com>
2022-03-25 14:04:23 -0400

    example for changing cover images

--- Web-Test/nav.js
+++ Web-Test/nav.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const button = document.querySelectorAll('button');
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -10,17 +10,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
 function hideShow() {
   let text = this.textContent;
   text = text.toLowerCase();
-  const home = document.getElementById("home");
+  const home = document.getElementById('home');
   const about = document.getElementById('about');
   const corpus = document.getElementById('corpus');
   const analysis = document.getElementById('analysis');
   const conclusion = document.getElementById('conclusion');
   const cur = document.getElementById(text);
   
-  home.style.display = "none";
-  about.style.display = "none";
-  corpus.style.display = "none";
-  analysis.style.display = "none";
-  conclusion.style.display = "none";
-  cur.style.display = "block";
+  if (text === 'about' | text === 'corpus' | text === 'analysis'){
+    document.getElementById('img').src = 'https://growingapologist.files.wordpress.com/2012/09/openbible-005-001.jpg';
+  }
+  else if (text === 'home'){
+    document.getElementById('img').src = 'https://g.christianbook.com/dg/product/cbd/f400/160341.jpg';
+  }
+  else {
+    document.getElementById('img').src = 'https://www.restorationlibrary.org/library/WWBB/WWBB_000_F_Cover%20Back.jpg';
+  }
+
+  home.style.display = 'none';
+  about.style.display = 'none';
+  corpus.style.display = 'none';
+  analysis.style.display = 'none';
+  conclusion.style.display = 'none';
+  cur.style.display = 'block';
 }
